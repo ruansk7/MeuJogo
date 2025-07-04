43e5f406c9722b56aba7c4772a237ae8c4927e6c
ruansk7 <ruansaulo7@gmail.com>
2025-07-04 19:08:41 -0300

    Paralax effect / Level1 implementation

--- code/Game.py
+++ code/Game.py
@@ -1,17 +1,23 @@
 import pygame
+from code.Const import MENU_OPTION, WIN_HEIGHT, WIN_WIDTH
 from code.Menu import Menu
+from code.Level import Level
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size = (600, 480))
+        self.window = pygame.display.set_mode(size = (WIN_WIDTH, WIN_HEIGHT))
         
     def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
-            #for event in pygame.event.get():
-               # if event.type == pygame.QUIT:
-              #      pygame.quit()
-              #      quit()
+            menu_return = menu.run()
+            
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[2]:
+                pygame.quit()
+                quit()
+            else:
+                pass
--- code/Menu.py
+++ code/Menu.py
@@ -5,7 +5,7 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
+from code.Const import C_CYAN, WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
 
 
 class Menu:
@@ -16,13 +16,13 @@ class Menu:
 
     def run(self):
         menu_option = 0
-        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.load('./asset/Menu.wav')
         pygame.mixer_music.play(-1)
         while True:
             # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, "Cyber", C_CYAN, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Jump", C_CYAN, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
@@ -34,20 +34,20 @@ class Menu:
             # Check for all events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()  # Close Window
-                    quit()  # end pygame
+                    pygame.quit()
+                    quit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_DOWN:  # DOWN KEY
+                    if event.key == pygame.K_DOWN:
                         if menu_option < len(MENU_OPTION) - 1:
                             menu_option += 1
                         else:
                             menu_option = 0
-                    if event.key == pygame.K_UP:  # UP KEY
+                    if event.key == pygame.K_UP:
                         if menu_option > 0:
                             menu_option -= 1
                         else:
                             menu_option = len(MENU_OPTION) - 1
-                    if event.key == pygame.K_RETURN:  # ENTER
+                    if event.key == pygame.K_RETURN:
                         return MENU_OPTION[menu_option]
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
